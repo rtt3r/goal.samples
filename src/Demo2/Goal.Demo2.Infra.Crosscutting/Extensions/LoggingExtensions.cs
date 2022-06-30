c364e6fc93ad17f900d6fa77cf6a9d20a3c9c193
Anderson Ritter de Souza <anderdsouza@gmail.com>
2022-06-29 23:49:05 -0300

    fix logging environment

--- src/Demo2/Goal.Demo2.Infra.Crosscutting/Extensions/LoggingExtensions.cs
+++ src/Demo2/Goal.Demo2.Infra.Crosscutting/Extensions/LoggingExtensions.cs
@@ -17,7 +17,7 @@ namespace Goal.Demo2.Infra.Crosscutting.Extensions
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails()
                 .Enrich.WithCorrelationId()
-                .Enrich.WithProperty("Environment", environment.EnvironmentName)
+                .Enrich.WithEnvironmentName()
                 .WriteTo.Debug(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
                 .WriteTo.Seq(configuration.GetConnectionString("Seq"))

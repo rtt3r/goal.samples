cbe4c32d8f9249f8ad9dc878ca5f98325db708fe
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-10-28 02:32:29 -0300

    its all working

--- dev-certs.ps1
+++ dev-certs.ps1
@@ -1,2 +1,2 @@
-dotnet dev-certs https -ep $env:APPDATA\Microsoft\UserSecrets\Development.pfx -p fd7ccf84-1ff8-4817-9ecd-f0a2047ea262
+dotnet dev-certs https -ep ~/.aspnet/https/Development.pfx -p fd7ccf84-1ff8-4817-9ecd-f0a2047ea262
 dotnet dev-certs https --trust
--- docker-up.sh
+++ docker-up.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
-
-docker-compose down
+docker-compose -f "docker-compose.yml" -f "docker-compose.$1.yml" down
 docker-compose -f "docker-compose.yml" -f "docker-compose.$1.yml" up -d --build --force-recreate --remove-orphans

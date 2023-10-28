e44975045ff0c893e74cbab6e185c0b4c6c50f10
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-10-27 22:52:26 -0300

    add osx docker support

--- docker-up.sh
+++ docker-up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
 docker-compose down
-docker-compose  -f "docker-compose.yml" -f "docker-compose.$1.yml" up -d --build --force-recreate --remove-orphans
+docker-compose -f "docker-compose.yml" -f "docker-compose.$1.yml" up -d --build --force-recreate --remove-orphans
